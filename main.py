ba3ace612dc1fa5dce5745863f49af4435f6db7c
Chasian <nathan.embaugh@gmail.com>
2021-06-17 22:34:15 -0400

    cleaned up dice rolling, made dice rolling function better and gives instructions for how to format

--- main.py
+++ main.py
@@ -1,25 +1,30 @@
 import discord
-import rolling_d20
+import rolling_d20 as roll
+
 
 class MyClient(discord.Client):
     async def on_ready(self):
         print('Logged on as', self.user)
-				
+
     async def on_message(self, message):
-        # don't respond to ourselves
+
+        # don't respond to ourselves, tho that is kind of fun
         if message.author == self.user:
             return
 
+        #Play ping pong!
         if message.content == 'ping':
             await message.channel.send('pong')
-	    
+
+        #Read and send dice roll commands
         if message.content.startswith('!r'):
-            await message.channel.send(rolling_d20.rolling_d20())
+            await message.channel.send(roll.rolling_d20(message.content))
+
+
 
 client = MyClient()
 
-with open('myToken.txt','r') as pwd:
+with open('myToken.txt', 'r') as pwd:
     token = pwd.read()
-
 
 client.run(token)
--- rolling_d20.py
+++ rolling_d20.py
@@ -1,8 +1,12 @@
 import d20
 
-def rolling_d20():
 
-    result =str(d20.roll("1d20"))
+def rolling_d20(content):
 
+    content_split = content.split()
+    if len(content_split) > 2 or len(content_split) < 2 :
+        return "Please enter a roll in the format '!r **X**d**Y**' where **X** and **Y** are numbers"
+    else:
+        result = str(d20.roll(str(content_split[-1])))
 
     return result
